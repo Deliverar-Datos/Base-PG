093b6b63192364e82f379005033d9b2766b9a047
jorge antonio segovia <jorgeantoniosegovia@MacBook-Air-de-jorge.local>
2025-05-17 18:31:25 -0300

    agregar col

---
+++ alembic/versions/aa93d5b140c4_agregar_columna.py
@@ -0,0 +1,27 @@
+"""agregar columna
+
+Revision ID: aa93d5b140c4
+Revises: f5a389d77dcb
+Create Date: 2025-05-17 18:29:22.705199
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'aa93d5b140c4'
+down_revision: Union[str, None] = 'f5a389d77dcb'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.add_column('usuarios', sa.Column('nueva_columna', sa.String(50)))
+    pass
+
+def downgrade():
+    op.drop_column('usuarios', 'nueva_columna')
+    pass
